88a75c6d70baf079eb4c3df8126c5f7bc376a5dc
Alex Forrence <alex.forrence@gmail.com>
2026-09-23 16:28:46 -0400

    Release v0.2.0

--- src/core/psychimgui_core.cpp
+++ src/core/psychimgui_core.cpp
@@ -42,7 +42,7 @@
 #  include <time.h>
 #endif
 
-#define PSYCHIMGUI_VERSION_STR "0.1.0"
+#define PSYCHIMGUI_VERSION_STR "0.2.0"
 
 #ifndef PSYCHIMGUI_BUILD_STR
 #  define PSYCHIMGUI_BUILD_STR "unknown build"
